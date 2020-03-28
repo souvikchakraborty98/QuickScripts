7324f5d0f388ff421c421be53edef3feae28587b
souvikchakraborty98 <souvik.chakraborty59@yahoo.com>
2020-03-28 19:54:44 +0530

    Added BugFix for nt platforms

--- test_sci.py
+++ test_sci.py
@@ -10,7 +10,7 @@ aborted=False
 link_reference_scihub = 'https://sci-hub.tw/10.1016/j.wocn.2018.07.001'
 link_reference="https://doi.org/10.1016/j.lwt.2020.109217"
 
-
+notFound=False
 
 # HEADER = '\033[95m'
 # OKBLUE = '\033[94m'
@@ -31,7 +31,7 @@ while aborted!=True:
     print(Fore.MAGENTA+"## SciHub Paper Downloader ## ver: 0.0.5a \n## Copyright,Souvik Chakraborty.The author in no way promotes \"SciHub\" or related parties.\nThis is just a scraping tool.##\n")
     print(Fore.MAGENTA+"Enter the DOI of the desired paper. Refer formats below:")
     print("Link Reference: "+link_reference+"\n"+"No. Reference: 10.3389/fimmu.2020.00693\n")
-    print(Fore.YELLOW+"Enter DOI url [CASE SENSITIVE]  O|R  DOI no. [CASE SENSITIVE] \n\n")
+    print(Fore.YELLOW+"Enter DOI url [CASE SENSITIVE]  O|R  DOI no. [CASE SENSITIVE]. You could paste it here. \n(For Windows platform, pre-Windows 10, refer \"Enabling copy paste in cmd\") \n\n")
     link=input()
 
     if link[0:4]=="http":
@@ -59,50 +59,61 @@ while aborted!=True:
              pdflist.append(url)
     except Exception as e:
         print(f"{Fore.RED}No links found: {e}")
+        notFound=True
         getch()
         exit()
 
     print("\nProbable Link[s]..\n")
-
+    newPdflist=[]
     for url in pdflist:
             if (".pdf" or "download=true" or "sci-hub") in str(url):
                 print(Fore.GREEN+"https:"+url[0:-1])
-
-    pdfDown="https:"+pdflist[0][0:-1]
-
-    # r1 = requests.get(pdfDown)
-    print("\nDownloading...Please wait..\n")
-    # fname = str(datetime.datetime.now().strftime("%f"))+".pdf"
-    fname=wget.download(pdfDown)
-    # print(fname)
-
-    # with open(fname, "wb") as mypdf:
-    #     mypdf.write(r1.content)
-        # total_length = int(r1.headers.get('content-length'))
-        # # print(total_length)
-        # for ch in progress.bar(r1.iter_content(chunk_size = 10240), expected_size=(total_length/1024) + 1):
-        #     if ch:
-        #         Pypdf.write(ch)
-
-    print(Fore.GREEN+"\n\nSaved to "+os.path.abspath(fname)+"\n")
+                newPdflist.append(str("https:"+url[0:-1]))
+    if len(newPdflist)<1:
+        print(f"{Fore.RED}No links found. Make sure you are entering DOI correctly / Requested file might not be present on SciHub.")
+        print(f"{Fore.BLUE}Press any key to continue..\n")
+        notFound=True
+        getch()
+    else:
+         pdfDown="https:"+pdflist[0][0:-1]
+         # r1 = requests.get(pdfDown)
+         print("\nDownloading...Please wait..\n")
+         # fname = str(datetime.datetime.now().strftime("%f"))+".pdf"
+         fname=wget.download(pdfDown)
+         # print(fname)
+         notFound=False
+         print(Fore.GREEN+"\n\nSaved to "+os.path.abspath(fname)+"\n")
+   
 
     op=True
     while op!=False:
-        print(Fore.GREEN+"\nPress 'Enter' to open. Press 'esc' to close.\n\nPress any other key to continue downloading...\n")
-        keypress=getch()
-        if keypress == chr(27).encode():
-            aborted=True
-            op=False
-            exit()
-        elif keypress == chr(13).encode():
-            os.startfile(fname)
-            print(Fore.BLUE+"Paused...Press any key to continue\n")
-            getch()
-            print(Style.BRIGHT+"\n==================================================\n")
-            op=True
-        else:
-            op=False
-            clear()
+        if notFound==False:
+            print(Fore.GREEN+"\nPress 'Enter' to open. Press 'esc' to close.\n\nPress any other key to continue downloading...\n")
+            keypress=getch()
+            if keypress == chr(27).encode():
+                aborted=True
+                op=False
+                exit()
+            elif keypress == chr(13).encode():
+                os.startfile(fname)
+                print(Fore.BLUE+"Paused...Press any key to continue\n")
+                getch()
+                print(Style.BRIGHT+"\n==================================================\n")
+                op=True
+            else:
+                op=False
+                clear()
+        elif notFound==True:
+            print(Fore.GREEN+"\nPress 'esc' to close.\n\nPress any other key to continue downloading...\n")
+            keypress=getch()
+            if keypress == chr(27).encode():
+                aborted=True
+                op=False
+                exit()
+            else:
+                op=False
+                clear()
+
          
 
 
